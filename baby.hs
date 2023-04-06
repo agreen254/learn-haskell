e46260600502cfb76d6c335a1ed99fdd2cc688dd
Aaron Green <aarongreen1997@gmail.com>
2023-04-06 03:23:49 -0500

    listz

--- baby.hs
+++ baby.hs
@@ -40,10 +40,30 @@ numsSeven = [1, 3 .. 21]
 
 numsEight = [20, 19 .. 1]
 
-compOne = [x*2 | x <- [1..10]]
+compOne = [x * 2 | x <- [1 .. 10]]
 
-compTwo = [x*2 | x <- [1..10], x*2 >= 12]
+compTwo = [x * 2 | x <- [1 .. 10], x * 2 >= 12]
 
-compThree = [x | x <- [50..100], x `mod` 7 == 3]
+compThree = [x | x <- [50 .. 100], x `mod` 7 == 3]
 
+-- weeding out lists by predicates is called `filtering`
 boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+ligmas = [ x*y*z | x <- [1, 2, 3], y <- [4, 5, 6], z <- [7, 8, 9]]
+
+nouns = ["Geezer", "Malcontent", "Ne'er-do-well"]
+adjectives = ["Dastardly", "Lugubrious", "Conniving"]
+
+expressions = [adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns]
+
+-- the _ means that we don't care what we draw from the list, so instead of
+-- declaring a variable name we will never use, we write _
+-- the function below replaces every element of a list with 1 and then sums them
+length' xs = sum [1 | _ <- xs]
+
+removeNonUpperCase st = [c | c <- st, c `elem` ['A'..'Z']]
+removeNonLowerCase st = [c | c <- st, c `elem` ['a'..'z']]
+
+-- can also do nested filtering, of course!!
+xxs = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]
+xxsFilt = [ [x | x <- xs, odd x] | xs <- xxs]
